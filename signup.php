2b250182b93a59af0aadd7369f508fa4d971247e
danhthanhnguyen <nguyendanhthanh980@gmail.com>
2021-05-26 23:26:40 +0700

    sign up

---
+++ signup.php
@@ -0,0 +1,25 @@
+<?php
+    $serverName = "localhost";
+    $userName = "root";
+    $password = "";
+    $dbname = "medicinedb";
+    // connect to db
+    $con = mysqli_connect($serverName, $userName, $password, $dbname);
+    if (isset($_POST["email"]) && isset($_POST["password"])) {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        // echo $password;
+        $sql = "insert into `users`( `email`, `password`, `isAdmin`)  values ('$email' , '$password', 0) ";
+        if (mysqli_query($con, $sql) == true) {
+            echo "You are now our member";
+        }
+        // echo $sql;
+        // $userInfo = mysqli_fetch_all($result);
+        // $isUser = mysqli_num_rows($result);
+        // if ($isUser > 0) {
+        //     $_SESSION["email"] = $userInfo[0][1];
+        //     echo $userInfo[0][1];
+
+        // }
+    }
+?>
