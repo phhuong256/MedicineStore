1d9c139e48d2ed4ed142856f75cb9f52c84177bb
danhthanhnguyen <nguyendanhthanh980@gmail.com>
2021-06-03 23:32:42 +0700

    fix: delete comments

--- js/comment.js
+++ js/comment.js
@@ -25,3 +25,31 @@ function typeComment() {
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send(`comments=&content=${comment.value.trim()}&id=${idProduct.value}`);
 }
+
+function deleteComment(target) {
+    let conf = confirm("Do you want to delete it?");
+    if(conf) {
+        xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if(this.readyState == 4 && this.status == 200) {
+                const cmt = JSON.parse(this.responseText);
+                console.log(cmt);
+                let listComment = "";
+                for (let i = 0; i < cmt.length; i++) {
+                    listComment += `<li class="list-group-item d-flex justify-content-between align-items-center">
+                                        ${cmt[i].content}
+                                        <span class="badge bg-primary rounded-pill">${cmt[i].email.split("@")[0]}</span>
+                                        <form class="row g-3" action="" method="post" enctype="multipart/form-data">
+                                            <input type="hidden" id="idProduct" name="idProduct" value="${cmt[i].id_product}">
+                                            <button type="button" value="${cmt[i].id_user}" onclick="deleteComment(this)" class="delete-comment btn btn-outline-danger btn-rounded"><i class="fas fa-trash"></i></button>
+                                        </form>
+                                    </li>`;
+                }
+                commentArea.innerHTML = listComment;
+            }
+        }
+        xhr.open("POST", "../deleteComment.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send(`delete=&idUser=${target.value}&id=${idProduct.value}`);
+    }
+}
