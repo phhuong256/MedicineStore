34467220c9080a74bcc1b2b7eb4b72b0ce627082
Vũ Phúc Trường An <56962455+truongan2000pro@users.noreply.github.com>
2021-06-03 12:00:13 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,4 +1,8 @@
+
+
 <?php
+
+
     session_start();
     $serverName = "localhost";
     $userName = "root";
@@ -15,9 +19,10 @@
         $userInfo = mysqli_fetch_all($result);
         $isUser = mysqli_num_rows($result);
         if ($isUser > 0) {
-            $_SESSION["email"] = $userInfo[0][0];
+            $_SESSION["email"] = $userInfo[0][1];
             echo $userInfo[0][1];
 
         }
     }
 ?>
+
